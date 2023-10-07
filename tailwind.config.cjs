805de7999a6d9bee1606e44d33414b9e3ae9848e
sylvhama <sylvhama@gmail.com>
2023-10-07 18:53:34 -0400

    add date animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,6 +19,48 @@ module.exports = {
       transitionProperty: {
         "opacity-transform": "opacity, transform",
       },
+      keyframes: {
+        slidein: {
+          from: {
+            transform: "translateX(2rem)",
+            opacity: 0,
+          },
+        },
+        rainbow: {
+          "0%": {
+            color: "hsl(0turn, 90%, 65%)",
+          },
+          "25%": {
+            color: "hsl(0.25turn, 90%, 65%)",
+          },
+          "50%": {
+            color: "hsl(0.5turn, 90%, 65%)",
+          },
+          "75%": {
+            color: "hsl(0.75turn, 90%, 65%)",
+          },
+          "100%": {
+            color: "hsl(1turn, 90%, 65%)",
+          },
+        },
+        floating: {
+          "0%": {
+            transform: "translateY(0px)",
+          },
+          "50%": {
+            transform: "translateY(5px)",
+          },
+          "100%": {
+            transform: "translateY(0px)",
+          },
+        },
+      },
+      animation: {
+        slidein:
+          "slidein 1s calc(var(--index) * 60ms) cubic-bezier(0.5, 0, 0.5, 1) both",
+        rainbow:
+          "rainbow 2s calc(var(--index) * 60ms) linear infinite, floating 1s calc(var(--index) * 60ms) ease-in-out infinite",
+      },
     },
   },
   plugins: [],
